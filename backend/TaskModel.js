aa28f2d25927d5f2d50b642f3a8ab970f901f8b3
nadigatlamahesh099 <97874345+nadigatlamahesh099@users.noreply.github.com>
2024-10-31 16:37:57 +0530

    TaskModel.js

--- backend/TaskModel.js
+++ backend/TaskModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  status: { type: String,default:"true"}
+  status: { type: String,default:false},
+  date:{type:String,required: true}
 });
 
 module.exports = mongoose.model('TaskModel', TaskSchema);
